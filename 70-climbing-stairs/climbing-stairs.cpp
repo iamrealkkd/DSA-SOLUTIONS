f9bc60a4891cabc9c78e558c454504474eeddd79
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2026-09-05 14:11:18 +0530

    Time: 2 ms (5.02%) | Memory: 8.5 MB (54.08%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,23 +1,12 @@
 class Solution {
 public:
-
+    int solve(int n, vector<int>&dp){
+        if(n <= 1) return 1;
+        if(dp[n] != -1)return dp[n];
+        return dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+    }
     int climbStairs(int n) {
-
-        // Step 1: Create DP table
-        vector<int> dp(n+1, -1);
-
-        // Step 2: Base Cases
-        dp[0]=1;
-        dp[1]=1;
-
-        // Step 3: Build answers from smaller states
-        for(int i=2;i<=n;i++){
-
-            dp[i]=dp[i-1]+dp[i-2];
-
-        }
-
-        // Step 4: Final Answer
-        return dp[n];
+        vector<int>dp(n + 1, -1);
+        return solve(n, dp);
     }
 };
