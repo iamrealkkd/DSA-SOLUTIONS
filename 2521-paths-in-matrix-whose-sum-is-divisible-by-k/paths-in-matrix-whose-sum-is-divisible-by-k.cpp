a0626b3fb549aff8fa9a4d8ad59248d1e882eeba
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2026-09-21 08:51:11 +0530

    Time: 199 ms (88.41%) | Memory: 132.1 MB (76.28%) - LeetSync

--- 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.cpp
+++ 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.cpp
@@ -2,36 +2,49 @@ class Solution {
 public:
     const int MOD = 1e9 + 7;
 
-    int solve(int i, int j, int m, int n, int k,
-              vector<vector<int>>& grid, int sum,
-              vector<vector<vector<int>>>& dp) {
-
-        if (i >= m || j >= n)
-            return 0;
-
-        sum += grid[i][j];
-        sum %= k;
-
-        if ((i == m - 1 && j == n - 1) && sum == 0)
-                return 1;
-
-        if (dp[i][j][sum] != -1)
-            return dp[i][j][sum];
-
-        return dp[i][j][sum] =
-            (solve(i + 1, j, m, n, k, grid, sum, dp) +
-             solve(i, j + 1, m, n, k, grid, sum, dp)) % MOD;
-    }
-
     int numberOfPaths(vector<vector<int>>& grid, int k) {
         int m = grid.size();
         int n = grid[0].size();
 
         vector<vector<vector<int>>> dp(
             m,
-            vector<vector<int>>(n, vector<int>(k, -1))
+            vector<vector<int>>(n, vector<int>(k, 0))
         );
 
-        return solve(0, 0, m, n, k, grid, 0, dp);
+        
+        for (int sum = 0; sum < k; sum++) {
+            int newSum = (sum + grid[m - 1][n - 1]) % k;
+
+            if (newSum == 0)
+                dp[m - 1][n - 1][sum] = 1;
+        }
+
+      
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+
+                if (i == m - 1 && j == n - 1)
+                    continue;
+
+                for (int sum = 0; sum < k; sum++) {
+
+                    int newSum = (sum + grid[i][j]) % k;
+
+                    int ans = 0;
+
+                    if (i + 1 < m) {
+                        ans += dp[i + 1][j][newSum];
+                    }
+
+                    if (j + 1 < n) {
+                        ans += dp[i][j + 1][newSum];
+                    }
+
+                    dp[i][j][sum] = ans % MOD;
+                }
+            }
+        }
+
+        return dp[0][0][0];
     }
 };
