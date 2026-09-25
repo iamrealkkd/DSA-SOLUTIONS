24ddd6d10b137449951d848b63b65e11563f9817
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2026-09-25 21:03:47 +0530

    Time: 3 ms (39.74%) | Memory: 15.2 MB (20.64%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,44 +1,47 @@
 class Solution {
 public:
 
-    int goWithPath(int i, int j, int m, int n, vector<vector<int>>& grid, vector<vector<int>>& dp) {
-
-        if(i < 0 || i >= m || j < 0 || j >= n)
-            return INT_MAX;
-
-        if(i == m - 1) {
-            return grid[i][j];
-        }
-
-        if(dp[i][j] != INT_MAX)
-            return dp[i][j];
-
-        int path1 = goWithPath(i + 1, j - 1, m, n, grid, dp);
-        int path2 = goWithPath(i + 1, j, m, n, grid, dp);
-        int path3 = goWithPath(i + 1, j + 1, m, n, grid, dp);
-
-        return dp[i][j] = grid[i][j] + min({
-            path1,
-            path2,
-            path3
-        });
-    }
-
     int minFallingPathSum(vector<vector<int>>& grid) {
 
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<vector<int>> dp(m, vector<int>(n, INT_MAX));
+        vector<vector<int>> dp(m, vector<int>(n));
+
+        // Last row copy
+        for(int j = 0; j < n; j++) {
+            dp[m - 1][j] = grid[m - 1][j];
+        }
+
+        // Bottom to top
+        for(int i = m - 2; i >= 0; i--) {
+
+            for(int j = 0; j < n; j++) {
+
+                int path1 = INT_MAX;
+                int path2 = INT_MAX;
+                int path3 = INT_MAX;
+
+                if(j - 1 >= 0)
+                    path1 = dp[i + 1][j - 1];
+
+                path2 = dp[i + 1][j];
+
+                if(j + 1 < n)
+                    path3 = dp[i + 1][j + 1];
+
+                dp[i][j] = grid[i][j] + min({
+                    path1,
+                    path2,
+                    path3
+                });
+            }
+        }
 
         int ans = INT_MAX;
 
         for(int j = 0; j < n; j++) {
-
-            ans = min(
-                ans,
-                goWithPath(0, j, m, n, grid, dp)
-            );
+            ans = min(ans, dp[0][j]);
         }
 
         return ans;
